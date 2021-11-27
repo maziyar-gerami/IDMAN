356fea9c2bf747605bdecb074ccb41dbecb3a6c0
smerdis <bardia.akbari@gmail.com>
2021-11-27 15:21:33 +0330

    Change Users Export Format from xlsx to xls

--- src/main/resources/static/js/Users/script.js
+++ src/main/resources/static/js/Users/script.js
@@ -1011,7 +1011,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     const url = window.URL.createObjectURL(new Blob([response.data]));
                     const link = document.createElement('a');
                     link.href = url;
-                    link.setAttribute("download", "users.xlsx");
+                    link.setAttribute("download", "users.xls");
                     document.body.appendChild(link);
                     link.click();
                 }).catch((error) => {
