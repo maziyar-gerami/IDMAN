fd0e5d21d2bbaca8f8ed532862cde6462ff6f784
maziyar grami <maziyar.grami@gmail.com>
2021-12-03 06:47:37 +0330

    Response class improved

--- src/main/java/parsso/idman/models/other/Response.java
+++ src/main/java/parsso/idman/models/other/Response.java
@@ -3,31 +3,30 @@ package parsso.idman.models.other;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.minidev.json.JSONObject;
+
 
 @Setter
 @Getter
 public class Response {
-    Result result;
+    Status result;
     Object data;
 
     public Response(Object data, String lang) {
-        this.result = new Result(lang);
+        this.result = new Status();
         this.data = data;
     }
 
     @Setter
     @Getter
-    private class Result{
+    private static class Status {
         int code;
-        String status;
+        String result_fa;
+        String result_en;
 
-        public Result(String lang) {
+        public Status() {
             code = 200;
-            if (lang.equals("en"))
-                status = StringResult.COMMON_200_EN;
-            else
-                status = StringResult.COMMON_200_FA;
+            result_en = StringResult.COMMON_200_EN;
+            result_fa = StringResult.COMMON_200_FA;
         }
     }
 }
