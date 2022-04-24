eff5d49538070d629086decec9773186fc1ed2ad
Maziyar Grami <Maziyar.Grami@gmail.com>
2022-04-24 16:55:00 +0430

    logout conroller fix

--- src/main/java/parsso/idman/controllers/PagesController.java
+++ src/main/java/parsso/idman/controllers/PagesController.java
@@ -1,11 +1,20 @@
 package parsso.idman.controllers;
 
 import io.jsonwebtoken.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.authentication.rememberme.AbstractRememberMeServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +41,30 @@ public class PagesController implements ErrorController {
   @GetMapping("/")
   public String root() {
     return "index";
+  }
+
+  @GetMapping("/resetpassword")
+  public String resetPass() {
+    return "redirect:/#/resetpassword";
+  }
+
+  @GetMapping("/changepassword")
+  public String changepassword() {
+    return "redirect:/#/changepassword";
+  }
+
+  @GetMapping("/logout")
+  public String logout(
+          HttpServletRequest request,
+          HttpServletResponse response,
+          SecurityContextLogoutHandler logoutHandler) {
+    Authentication auth = SecurityContextHolder
+            .getContext().getAuthentication();
+    logoutHandler.logout(request, response, auth);
+    new CookieClearingLogoutHandler(
+            AbstractRememberMeServices.SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY)
+            .logout(request, response, auth);
+    return "redirect:" + casLogout;
   }
 
   
