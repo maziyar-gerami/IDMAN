87ed52784232423abbc06d57547b94c969b0652b
Maziyar Grami <Maziyar.Grami@gmail.com>
2022-05-02 10:32:05 +0430

    NEW FRONT

--- src/main/resources/static/js/chunk-2d21f857.618b1e6d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21f857"],{d9d0:function(c,e,s){"use strict";s.r(e);var t=s("7a23"),n={class:"grid"},d={class:"col-12"},a={class:"card"};function r(c,e,s,r,o,b){return Object(t["C"])(),Object(t["j"])("div",n,[Object(t["k"])("div",d,[Object(t["k"])("div",a,[Object(t["k"])("h3",null,Object(t["O"])(c.$t("accessReports")),1)])])])}var o={name:"AccessReports"},b=s("6b0d"),i=s.n(b);const u=i()(o,[["render",r]]);e["default"]=u}}]);
-//# sourceMappingURL=chunk-2d21f857.618b1e6d.js.map
