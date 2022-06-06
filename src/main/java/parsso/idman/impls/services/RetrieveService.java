87d7d09cbb54b66bcf9a117fbe524563efe3b2ee
Maziyar Grami <Maziyar.Grami@gmail.com>
2022-06-06 16:16:31 +0430

    user services minor fix

--- src/main/java/parsso/idman/impls/services/RetrieveService.java
+++ src/main/java/parsso/idman/impls/services/RetrieveService.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 import javax.servlet.http.HttpServletResponse;
 import lombok.val;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +18,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import parsso.idman.helpers.Settings;
 import parsso.idman.helpers.UniformLogger;
 import parsso.idman.helpers.Variables;
-import parsso.idman.helpers.service.Notifs;
 import parsso.idman.impls.services.create.sublcasses.Analyze;
 import parsso.idman.models.logs.ReportMessage;
 import parsso.idman.models.services.Service;
@@ -28,8 +26,6 @@ import parsso.idman.models.services.serviceType.SimpleService;
 import parsso.idman.models.services.servicesSubModel.ExtraInfo;
 import parsso.idman.models.users.User;
 import parsso.idman.repos.ServiceRepo;
-
-
 
 @org.springframework.stereotype.Service
 
@@ -50,43 +46,50 @@ public class RetrieveService implements ServiceRepo.Retrieve {
 
   }
 
-  public List<MicroService> userServices(User user) {
+  @Override
+  public List<MicroService> listUserServices(User user) {
+
+    Logger logger = LoggerFactory.getLogger(user.get_id().toString());
 
     List<Service> services = listServicesFull();
 
-    LinkedList<Service> relatedList = new LinkedList<>();
+    List<Service> relatedList = new LinkedList<>();
 
     for (Service service : services) {
 
       if (service.getAccessStrategy() != null)
-        if (service.getAccessStrategy().getRequiredAttributes() != null)
+        if (service.getAccessStrategy().getRequiredAttributes() != null) {
           if (service.getAccessStrategy().getRequiredAttributes().get("ou") != null) {
 
             Object member = service.getAccessStrategy().getRequiredAttributes().get("ou");
             if (member != null) {
               JSONArray s = (JSONArray) member;
 
-              if (user.getMemberOf() != null)
+              if (user.getMemberOf() != null && s != null)
                 for (int i = 0; i < user.getMemberOf().size(); i++)
                   for (int j = 0; j < ((JSONArray) s.get(1)).size(); j++) {
                     if (user.getMemberOf().get(i).equals(((JSONArray) s.get(1)).get(j))
-                        && !relatedList.contains(service)) {
+                        && !relatedList.contains(service) && service.getAccessStrategy().isEnabled()) {
                       relatedList.add(service);
-                      break;
 
                     }
                   }
             }
 
           }
+          if (service.getAccessStrategy().getRequiredAttributes().get("uid") != null) {
+            Object memberUid = service.getAccessStrategy().getRequiredAttributes().get("uid");
+            if (memberUid != null) {
+              JSONArray s = (JSONArray) memberUid;
 
-      try {
-        if (((List<String>) (((JSONArray) (Objects.requireNonNull(service.getAccessStrategy())
-            .getRequiredAttributes().get("uid"))).get(1))).contains(user.get_id()))
-          relatedList.add(service);
-      } catch (NullPointerException ignored) {
+              if (((List<String>) s.get(1)).contains(user.get_id()) && !relatedList.contains(service)
+                  && service.getAccessStrategy().isEnabled()) {
+                relatedList.add(service);
 
-      }
+              }
+            }
+          }
+        }
 
     }
     List<MicroService> microServices = new LinkedList<>();
@@ -95,98 +98,13 @@ public class RetrieveService implements ServiceRepo.Retrieve {
     for (Service service : relatedList) {
       Query query = new Query(Criteria.where("_id").is(service.getId()));
       try {
-        JSONObject jsonObject = mongoTemplate.findOne(query, JSONObject.class, Variables.col_servicesExtraInfo);
-        microService = new MicroService((Long) jsonObject.get("_id"), jsonObject.get("url").toString(),
-            Integer.parseInt(jsonObject.get("position").toString()));
+        microService = mongoTemplate.findOne(query, MicroService.class, Variables.col_servicesExtraInfo);
       } catch (Exception e) {
-        e.printStackTrace();
         microService = new MicroService(service.getId(), service.getServiceId());
-        uniformLogger.error(Variables.DOER_SYSTEM,
-            new ReportMessage(Variables.MODEL_SERVICE, service.getId(), "",
-                Variables.ACTION_RETRIEVE, Variables.RESULT_FAILED,
-                "unable read extra info from mongo"));
-
+        logger.warn("Unable to read ExtraInfo for service " + service.getId());
       } finally {
-        MicroService fMicro = new MicroService(service, microService);
-
-        try {
-          fMicro.setNotification(new Notifs().getNotifications(user.get_id().toString(),
-              service.getExtraInfo().getNotificationApiURL(),
-              service.getExtraInfo().getNotificationApiKey()));
-        } catch (Exception ignored) {
-        }
-
-        microServices.add(fMicro);
-
+        microServices.add(new MicroService(service, microService));
       }
-    }
-
-    Collections.sort(microServices);
-    return microServices;
-
-  }
-
-  @Override
-  public List<MicroService> listUserServices(User user) {
-
-  Logger logger = LoggerFactory.getLogger(user.get_id().toString());
-
-    
-    List<Service> services = listServicesFull();
-
-    List<Service> relatedList = new LinkedList<>();
-
-    for (Service service : services) {
-
-        if (service.getAccessStrategy() != null)
-            if (service.getAccessStrategy().getRequiredAttributes() != null){
-                if (service.getAccessStrategy().getRequiredAttributes().get("ou") != null) {
-
-                    Object member = service.getAccessStrategy().getRequiredAttributes().get("ou");
-                    if (member != null) {
-                        JSONArray s = (JSONArray) member;
-
-                        if (user.getMemberOf() != null && s != null)
-                            for (int i = 0; i < user.getMemberOf().size(); i++)
-                                for (int j = 0; j < ((JSONArray) s.get(1)).size(); j++) {
-                                    if (user.getMemberOf().get(i).equals(((JSONArray) s.get(1)).get(j)) && !relatedList.contains(service) && service.getAccessStrategy().isEnabled()) {
-                                        relatedList.add(service);
-                                        break;
-
-                                    }
-                                }
-                    }
-
-
-                }if (service.getAccessStrategy().getRequiredAttributes().get("uid") != null) {
-                  Object memberUid = service.getAccessStrategy().getRequiredAttributes().get("uid");
-                  if (memberUid != null) {
-                    JSONArray s = (JSONArray) memberUid;
-      
-                    if (((List<String>) s.get(1)).contains(user.get_id()) && !relatedList.contains(service)&& service.getAccessStrategy().isEnabled()) {
-                      relatedList.add(service);
-                      break;
-      
-                    }
-                  }
-                }
-              }
-
-
-    }
-    List<MicroService> microServices = new LinkedList<>();
-    MicroService microService = null;
-
-    for (Service service : relatedList) {
-        Query query = new Query(Criteria.where("_id").is(service.getId()));
-        try {
-            microService = mongoTemplate.findOne(query, MicroService.class, Variables.col_servicesExtraInfo);
-        } catch (Exception e) {
-            microService = new MicroService(service.getId(), service.getServiceId());
-            logger.warn("Unable to read ExtraInfo for service " + service.getId());
-        } finally {
-            microServices.add(new MicroService(service, microService));
-        }
     }
 
     Collections.sort(microServices);
@@ -199,21 +117,21 @@ public class RetrieveService implements ServiceRepo.Retrieve {
     String[] files = folder.list();
     List<Service> services = new LinkedList<>();
     Service service = null;
-    if(files!=null)
-    for (String file : Objects.requireNonNull(files)) {
-      if (file.endsWith(".json"))
-        try {
-          service = new Analyze(mongoTemplate, new RetrieveService(mongoTemplate), uniformLogger)
-              .analyze(file);
-          services.add(service);
-          Collections.sort(services);
-        } catch (Exception e) {
-          e.printStackTrace();
-          uniformLogger.warn(Variables.DOER_SYSTEM,
-              new ReportMessage(Variables.MODEL_SERVICE, service.getId(),
-                  "", Variables.ACTION_RETRIEVE, Variables.RESULT_FAILED, "Unable to read service"));
-        }
-    }
+    if (files != null)
+      for (String file : Objects.requireNonNull(files)) {
+        if (file.endsWith(".json"))
+          try {
+            service = new Analyze(mongoTemplate, new RetrieveService(mongoTemplate), uniformLogger)
+                .analyze(file);
+            services.add(service);
+            Collections.sort(services);
+          } catch (Exception e) {
+            e.printStackTrace();
+            uniformLogger.warn(Variables.DOER_SYSTEM,
+                new ReportMessage(Variables.MODEL_SERVICE, service.getId(),
+                    "", Variables.ACTION_RETRIEVE, Variables.RESULT_FAILED, "Unable to read service"));
+          }
+      }
     return services;
   }
 
@@ -237,14 +155,14 @@ public class RetrieveService implements ServiceRepo.Retrieve {
   }
 
   @Override
-  public List<SimpleService> listServicesMain(String page , String count) {
+  public List<SimpleService> listServicesMain(String page, String count) {
     Query query = new Query();
-    
+
     if (!page.equals("") && !page.equals(""))
       query.skip(Integer.parseInt(page)).limit(Integer.parseInt(count));
-      
-    return mongoTemplate.find(query.with(Sort.by(Sort.Direction.DESC, "position"))
-    , SimpleService.class, Variables.col_servicesExtraInfo);
+
+    return mongoTemplate.find(query.with(Sort.by(Sort.Direction.DESC, "position")), SimpleService.class,
+        Variables.col_servicesExtraInfo);
   }
 
   @Override
